5ba32aa4490e192f635645ce2570ba193e6c87f2
andrew.nash <akahadaka@gmail.com>
2017-01-12 18:34:08 +0200

    ng generate service services/auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 //angular
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule }  from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
+//libraries
+import { AuthService }    from './services/auth.service';
+
 //app
-import { AppComponent } from './app.component';
+import { AppComponent }   from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAeP2LufplZQu8plXZkV0C2FvHLI7d88lQ",
+  authDomain: "jink-66278.firebaseapp.com",
+  databaseURL: "https://jink-66278.firebaseio.com",
+  storageBucket: "jink-66278.appspot.com",
+  messagingSenderId: "430419991991"
+}
 
 @NgModule({
   declarations: [
@@ -18,7 +29,11 @@ import { AppComponent } from './app.component';
     HttpModule,
     MaterialModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    AuthService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,30 @@
+// angular
+import { Injectable }                             from '@angular/core';
+
+// library
+import {AngularFire, AuthProviders, AuthMethods}  from 'angularfire2';
+
+@Injectable()
+export class AuthService {
+
+  constructor(public af:AngularFire) { }
+
+  /**
+   * Logs in the user
+   * @returns {firebase.Promise<FirebaseAuthState>}
+   */
+  loginWithGoogle() {
+    return this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup,
+    });
+  }
+
+  /**
+   * Logs out the current user
+   */
+  logout() {
+    return this.af.auth.logout();
+  }
+
+}
